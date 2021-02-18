acd36a0f0400d4b1bdaa995b9b8c33e5463c76fb
Jose Hurtado <josehumberto17@gmail.com>
2021-02-18 18:00:39 -0500

    Update just cucumber

--- src/main/java/com/company/utils/Urls.java
+++ src/main/java/com/company/utils/Urls.java
@@ -2,6 +2,6 @@ package com.company.utils;
 
 public class Urls {
 
-    public static final String REDMINE_URL_LOCAL = "http://localhost:8081";
+    public static final String REDMINE_URL_LOCAL = "http://198.211.98.120:8081";
     public static final String REDMINE_URL_CERTIFICACION = "http://localhost:8081";
 }
